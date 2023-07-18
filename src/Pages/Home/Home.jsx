4b227395edd6856a1ae8b54883fd0bb9f81eb06a
tarunkridafans <tarun@krida.fans>
2023-07-18 11:03:10 +0530

    fix-repsonsiveness

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,13 +3,13 @@ import logo from "../../Assets/logop.png";
 
 function Home() {
   return (
-    <main className="flex flex-col md:flex-row justify-center  items-center mr-40 ml-40 height-auto flex-1 mb-28 ">
-      <section className="md:w-[50%] mt-8 md:mt-0">
-        <h2 className="text-6xl mb-6 gradient-text">
+    <main className="flex flex-col md:flex-row justify-center  items-center md:mr-40 md:ml-40 height-auto flex-1 mb-28 ">
+      <section className="md:w-[50%] mt-8 md:mt-0 p-6">
+        <h2 className=" text-[2rem] md:text-6xl mb-6 gradient-text">
           Rediscover Lost Possessions Swiftly - FindInABlink, Your Local-First
           App!
         </h2>
-        <p className="text-xl text-[#A8A196] font-[600]">
+        <p className="text-lg md:text-xl text-[#A8A196] font-[600]">
           FindInABlink - Where lost items find their way home. Our compassionate
           community and smart search tools make the process seamless. Reclaim
           what's yours swiftly and rediscover peace of mind today!
